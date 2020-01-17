b15ef00d89155539aba6e849f94af3732d485dee
sebastienlanglois <sebastien.langlois@polymtl.ca>
2020-01-17 07:21:00 -0500

    setup.py initial file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+PYTHON_REQUIRES = ">=3.6"
+
+setup(name='hydrotools',
+      version='1.0',
+      python_requires=PYTHON_REQUIRES,
+      packages=find_packages(),
+      )
